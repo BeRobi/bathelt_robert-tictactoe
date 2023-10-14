2a66b35d798e2cc2eefeea69f1b1f24ea27c6d87
BeRobi <screengsm@gmail.com>
2023-10-14 09:33:10 +0200

    div elem megjelenítése

---
+++ Elem.js
@@ -0,0 +1,21 @@
+class Elem {
+  // az osztályban az adattagokon végezzük a műveleteket
+  // megjelenítse az oldalon az elemet
+  constructor(szuloElem) {
+    // beállítja az adattagokat
+    this.szuloElem = szuloElem;
+    this.htmlOsszeallit(); // a this - konkrét osztálypéldányra mutat
+    // megfogjuk a html elemekhez
+    this.divElem = this.szuloElem.children("div")
+    this.pElem = this.divElem.children("p")
+    console.log(this.divElem)
+    console.log(this.pElem)
+  }
+
+  htmlOsszeallit() {
+    let txt = "<div class='elem'><p></p></div>";
+    this.szuloElem.append(txt);
+  }
+}
+
+export default Elem;
---
+++ main.js
@@ -0,0 +1,7 @@
+import Elem from "./Elem.js"
+
+$(function(){
+
+    //Példányosítjuk az osztályunkat
+    new Elem($(".jatekter"))
+})
